855856c0542fab66541d17d4758325825b82902c
Jason <Jason078524@gmail.com>
2022-09-03 17:23:15 +0800

    second commit
    1. add yaml converter
    2. add reg file
    3. add launch.json
    4. change naming
    5. add const variable maker

--- chatParser.py
+++ chatParser.py
@@ -18,61 +18,135 @@
 # json file
 
 
-import json, time
-from pkgutil import extend_path
-# from time import strftime,gmtime
+from pathlib import Path
+import yaml
+import json
+import time
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile
 
-# Opening JSON file
-folderPath = './'
+'''
+    Configs
+'''
+# target folder (workspace)
+import sys
 
 
-def translateToMarkdown(folderPath, fileName):
-    filePath = join(folderPath, fileName)
-    f = open(filePath + ".json", "r", encoding="utf-8")
+class Const(object):
+    """
+        Manually reconstruct an usage of constant variables
+    """
+    def ConstError(TypeException, text):
+        print(text)
+        pass
 
-    # returns JSON object as
-    # a dictionary
+    def __setattr__(self, key, value):
+        if self.__dict__.has_key(key):
+            raise self.ConstError("Changing const.%s", key)
+        else:
+            self.__dict__[key] = value
+
+    def __getattr__(self, key):
+        if self.__dict__.has_key(key):
+            return self.key
+        else:
+            return None
+
+    # constant variables
+    # You can use os.path.abspath() to convert it:
+    FOLDER_PATH = Path(r"C:\Users\Jason\Downloads\TwitchDownloader\chat")
+
+
+consts = Const()
+
+
+'''
+    Global Variables
+'''
+DICT_json_files = {}
+DICT_markdown_files = {}
+
+'''
+    Functions
+'''
+
+
+def dump_into_yaml_file(data, file):
+    """
+    Convert a python object into yaml
+
+    Args:
+        data (any): python objected data
+        file (string): file name with full path
+
+    """
+    # bonus, convert json into yaml
+    with open(file, 'w') as f:
+        yaml.dump(data, f)
+
+
+def translate_to_markdown(folder_path, file_name):
+    json_file = folder_path / (file_name + ".json")
+    print(json_file)
+    f = open(json_file, "r", encoding="utf-8")
+
+    # returns JSON object as a dictionary
     data = json.load(f)
 
-    # Closing file
+    # Close reading file
     f.close()
-
-    f = open(filePath + ".md", "w", encoding="utf-8")
+    markdown_file_path = folder_path / "out"
+    markdown_file = markdown_file_path / (file_name + ".md")
+    f = open(markdown_file, "w", encoding="utf-8")
 
     # Iterating through the json
     # list
     for row in data['comments']:
-        offsetSeconds = row['content_offset_seconds']
-        timeStamp = time.strftime('%H:%M:%S', time.gmtime(offsetSeconds))
+        offset_seconds = row['content_offset_seconds']
+        timeStamp = time.strftime('%H:%M:%S', time.gmtime(offset_seconds))
 
         name = row['commenter']['display_name']
         msg = row['message']['body'].replace("*", "\*")
-        userColor = row['message']['user_color']
+        user_color = row['message']['user_color']
         # f.write(f'[{timeStamp}][{name}]: {msg}')
         f.write(f' {timeStamp} ')
-        f.write(f'<span style="color:{userColor}">{name}</span>: {msg}')
+        f.write(f'<span style="color:{user_color}">{name}</span>: {msg}')
         f.write('  \n')
 
     f.close()
 
+    # bonus, dump into a yaml file
+    yaml_file_path = folder_path / "out"
+    yaml_file = yaml_file_path / (file_name + ".yaml")
+    dump_into_yaml_file(data, yaml_file)
 
-jsonFiles = {}
-markdownFiles = {}
 
-for fileName in listdir(folderPath):
-    fullPathFileName = join(folderPath, fileName)
-    if isfile(fullPathFileName):
-        if fileName[-5:] == ".json":
-            jsonFiles[fileName[:-5]] = True
-        elif fileName[-3:] == ".md":
-            markdownFiles[fileName[:-3]] = True
+'''
+    Main Code
+'''
+# src
+# JSON files
+for file_name in listdir(consts.FOLDER_PATH):
+    file = consts.FOLDER_PATH / file_name
+    if isfile(file):
+        if file_name[-5:] == ".json":
+            print(file)
+            DICT_json_files[file_name[:-5]] = True
 
-for fileName in jsonFiles:
-    if fileName not in markdownFiles:
-        print(f'generating files: {fileName} ....')
-        translateToMarkdown(folderPath, fileName)
+# dst
+# markdown outputs
+src_folder = consts.FOLDER_PATH / "out"
+for file_name in listdir(src_folder):
+    file = consts.FOLDER_PATH / file_name
+    if isfile(file):
+        if file_name[-3:] == ".md":
+            DICT_markdown_files[file_name[:-3]] = True
+
+# generate output files
+for file_name in DICT_json_files:
+    if file_name not in DICT_markdown_files:
+        print(f'generating files: {file_name} ....')
+        translate_to_markdown(consts.FOLDER_PATH, file_name)
         print(f'done\n')
 
 print('finish\n')
